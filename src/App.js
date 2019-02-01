d5b006407babda77068df870aed077f25ad90cbc
2otogizoshi <47193542+2otogizoshi@users.noreply.github.com>
2019-02-01 17:15:06 -0600

    Finished chalenge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      answer: "yes",
+      name: "bob",
+      age: "19"
+    };
+  }
   render() {
-    return <div>Helo</div>;
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age} years old</h3>
+      </div>
+    );
   }
 }
 
